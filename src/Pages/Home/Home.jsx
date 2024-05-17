ea5b7c7b7b72583a6fe65eca07f78235aa617575
Idba1 <moniraislamself@gmail.com>
2024-05-17 18:58:46 +0600

    reviews json

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Featured from "../Featured/Featured";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import PopulerMenu from "./PopularMenu/PopulerMenu";
+import Testimonial from "./Testimonial/Testimonial";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Category></Category>
             <PopulerMenu></PopulerMenu>
             <Featured></Featured>
+            <Testimonial></Testimonial>
         </div>
     );
 };
