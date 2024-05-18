62ed32d65cc19cc54da05c2615ddef3e6b3ff701
Idba1 <moniraislamself@gmail.com>
2024-05-19 01:08:52 +0600

    add food card and show all data in specific tab section

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -4,16 +4,24 @@ import orderCoverImg from "../../../assets/shop/banner2.jpg"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import useMenu from "../../../Hooks/Menu/useMenu";
+import FoodCard from "../../../Components/FoodCard/FoodCard";
 
 const Order = () => {
     const [menu] = useMenu();
+
+
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const drinks = menu.filter(item => item.category === 'drinks');
     return (
         <div>
             <Helmet>
                 <title>BISTRO BOSS | ORDER</title>
             </Helmet>
             <Cover img={orderCoverImg} title="Order Food"></Cover>
-            <Tabs>
+            <Tabs className="my-7 md:my-10 lg:my-16">
                 <TabList>
                     <Tab>Salad</Tab>
                     <Tab>Pizza</Tab>
@@ -23,19 +31,54 @@ const Order = () => {
                 </TabList>
 
                 <TabPanel>
-                    <h2>Salad</h2>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            salad.map(item => <FoodCard
+                                key={item._id}
+                                item={item}>
+                            </FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Pi</h2>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            pizza.map(item => <FoodCard
+                                key={item._id}
+                                item={item}>
+                            </FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 3</h2>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            soup.map(item => <FoodCard
+                                key={item._id}
+                                item={item}>
+                            </FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 4</h2>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            desserts.map(item => <FoodCard
+                                key={item._id}
+                                item={item}>
+                            </FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 4</h2>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            drinks.map(item => <FoodCard
+                                key={item._id}
+                                item={item}>
+                            </FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
             </Tabs>
         </div>
