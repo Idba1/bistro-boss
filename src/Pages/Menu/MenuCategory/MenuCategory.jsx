d7df885b02b5340a039cede85280b66f8ac9aaaa
Idba1 <moniraislamself@gmail.com>
2024-05-18 23:04:19 +0600

    update button

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,17 +1,22 @@
 import Cover from "../../Shared/Cover/Cover";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = ({ items ,title, img}) => {
+const MenuCategory = ({ items, title, img }) => {
     return (
         <div className='pt-8'>
-             { title && <Cover img={img} title={title}></Cover>}
-            <div className="grid md:grid-cols-2 gap-10 my-16">
-                {
-                    items.map(item => <MenuItem
-                        key={item._id}
-                        item={item}
-                    ></MenuItem>)
-                }
+            <div>
+                {title && <Cover img={img} title={title}></Cover>}
+                <div className="grid md:grid-cols-2 gap-10 my-16">
+                    {
+                        items.map(item => <MenuItem
+                            key={item._id}
+                            item={item}
+                        ></MenuItem>)
+                    }
+                </div>
+            </div>
+            <div className="border-6">
+                <button className="btn flex mx-auto text-center my-8 btn-outline border-0 border-b-4 mt-4">Order Now</button>
             </div>
         </div>
     );
