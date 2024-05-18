b01b2432f741957a14ad2a285cadb65053763f76
Idba1 <moniraislamself@gmail.com>
2024-05-18 23:18:50 +0600

    order banner set

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,11 +1,14 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import orderCoverImg from "../../../assets/shop/banner2.jpg"
 
 const Order = () => {
     return (
         <div>
             <Helmet>
-                <title>HOME | ORDER</title>
+                <title>BISTRO BOSS | ORDER</title>
             </Helmet>
+            <Cover img={orderCoverImg} title="Order Food"></Cover>
         </div>
     );
 };
