4f2bacf2a9efb90f2ddfec1606ad25113ba14e89
Idba1 <moniraislamself@gmail.com>
2024-05-19 00:51:59 +0600

    implement react tab section

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,14 +1,43 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import orderCoverImg from "../../../assets/shop/banner2.jpg"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from "../../../Hooks/Menu/useMenu";
 
 const Order = () => {
+    const [menu] = useMenu();
     return (
         <div>
             <Helmet>
                 <title>BISTRO BOSS | ORDER</title>
             </Helmet>
             <Cover img={orderCoverImg} title="Order Food"></Cover>
+            <Tabs>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Salad</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Pi</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 4</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 4</h2>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
